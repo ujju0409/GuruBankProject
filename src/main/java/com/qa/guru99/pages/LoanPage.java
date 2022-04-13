0983165b336313f75064f0169ac9ff780e03de54
Ujjval Patel <ujjvalpatel@Ujjvals-MBP.ht.home>
2022-04-13 03:34:41 -0400

    loan page is add

---
+++ src/main/java/com/qa/guru99/pages/LoanPage.java
@@ -0,0 +1,14 @@
+package com.qa.guru99.pages;
+
+public class LoanPage {
+
+	public void  getLoan() {
+		System.out.println(" get loan from the bank  ");
+	}
+	
+	public void interset() {
+		System.out.println("calculate the your interset rate ");
+	}
+	
+	
+}
